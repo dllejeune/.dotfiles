b64c7994c5e96b4af590bb446061663da4d91167
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2022-07-29 18:30:47 +0200

    test(vendor): installe les dépendance avec le ubuntu-setup plutot que dotbot

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -15,4 +15,11 @@ mkdir ${HOME}/bin
 PATH="${HOME}/bin:${PATH}"
 ln -s /usr/bin/python3.9 ${HOME}/bin/python
 
+git submodule update --init --recursive
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
 ./install
+
