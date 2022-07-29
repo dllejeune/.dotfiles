3482a16a011c919edc1e7015ed6882d90c1e3341
phreno <losgoomy@gmail.com>
2022-07-29 22:58:37 +0200

    fix(license): corrige une typo dans la license

--- ubuntu/ubuntu-setup.sh
+++ ubuntu/ubuntu-setup.sh
@@ -16,20 +16,20 @@
 # Copyright 2022 K3rn€l P4n1k
 # ===========================
 #
-# This file is part of ubuntu-setup.sh.
+# This file is part of phreno-dot-files.
 #
-# ubuntu-setup.sh is free software: you can redistribute it and/or modify
+# phreno-dot-files is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ubuntu-setup.sh is distributed in the hope that it will be useful,
+# phreno-dot-files is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ubuntu-setup.sh. If not, see <http://www.gnu.org/licenses/>.
+# along with phreno-dot-files. If not, see <http://www.gnu.org/licenses/>.
 
 # ------------------
 # VARIABLES GLOBALES
