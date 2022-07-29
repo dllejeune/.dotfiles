82cb35f5399ec6eb8393917ab2876b2f09cfabf1
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2022-07-29 17:58:15 +0200

    test(python): ajoute le ppa vers la version 3.9

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+add-apt-repository ppa: deadsnakes / ppa
+
 apt-get -y install \
     cmake  \
     tmux   \
     neovim \
     zsh    \
     git    \
-    python3
+    python3.9
 
+# make that damn dirty snake available
+mkdir ${HOME}/bin
+PATH="$HOME/bin:$PATH"
 ln -s /usr/bin/python3 ~/bin/python
 
 ./install
