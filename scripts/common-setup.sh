13f198d601fefce9598b157ef0c24b9b400720c2
phreno <losgoomy@gmail.com>
2022-07-30 14:43:16 +0200

    fix(nvm): fix l'ordre des commandes permettant le chargement de nvm

--- scripts/common-setup.sh
+++ scripts/common-setup.sh
@@ -128,10 +128,10 @@ installe_vim_awesome(){
 installe_node(){
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     nvm install node
-    nvm use node
+    export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    export NVM_DIR="$HOME/.nvm"
+    nvm use node
 }
 
 installe_vimrc(){
