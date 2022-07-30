b0a625d1f9d8d404c1326a2c0f97d1d50da9a471
phreno <losgoomy@gmail.com>
2022-07-30 14:38:12 +0200

    fix(common): charge nvm avant de pouvoir l'utiliser

--- scripts/common-setup.sh
+++ scripts/common-setup.sh
@@ -129,6 +129,9 @@ installe_node(){
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     nvm install node
     nvm use node
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    export NVM_DIR="$HOME/.nvm"
 }
 
 installe_vimrc(){
